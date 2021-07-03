2523bb8e069c8b9ddc12e213925b3b4d52329c87
ShanSharma08 <shans29008@gmail.com>
2021-07-03 18:00:26 +0530

    edit 1

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
-import Hs from './screens/Hs'
-
+import Hs from './screens/Hs';
+import dictionary from './screens/database';
 
 export default class App extends Component{
   render(){
     return(
       <View style={{flex:1}}>
         <Hs />
-      </View>
+      </View> 
     )
   }
 }
